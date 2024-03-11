13171ef389272262eecf77f43d2ab1a8adeaa6c2
Savinda Lenaduwa <124413767+slen42@users.noreply.github.com>
2024-03-11 20:21:03 +1300

    complete functioning

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const handleOnSerachChange = (searchData) => {
   const [lat, lon] = searchData.value.split(" ");
 
   const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
-  const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}units=metric`)
+  const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
 
 
   Promise.all([currentWeatherFetch, forecastFetch])
